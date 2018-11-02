6ef8b6e8abc94ab192e55f6972f956688c366f5e
Kevin Chen <kchen01@stuy.edu>
2018-11-02 14:27:59 -0400

    no errors :)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
-import java.util.List;
 public class NoNullArrayList<T> extends ArrayList<T> {
     private T value;
-    
-
-
-
-
-
-
-
-
-
-
+    public NoNullArrayList(){
+      super();
+    }
+    public NoNullArrayList(int capacity){
+      super(capacity);
+    }
+    public boolean add(T value) {
+      if (value == null) {
+        throw new IllegalArgumentException("cannot set a value to null");
+      }
+      return super.add(value);
+      }
+    public T set(int a,T value) {
+      if (value == null) {
+        throw new IllegalArgumentException("cannot set a value to null");
+      }
+      return super.set(a,value);
+    }
 
 
 }
